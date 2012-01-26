0ad16d77828e51ab082cac162a306593425d5f46
Peter Hamilton <peter@inspiredpixel.net>
2012-01-26 04:32:34 +0000

    Neatened up the intial tests:

--- spec/citier_base_spec.rb
+++ spec/citier_base_spec.rb
@@ -2,40 +2,40 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
-describe "Extending active record methods" do
-
-  before :all do
-    ActiveRecord::Schema.define do
-      create_table :citier_classes do |t|
-      end
-      create_table :non_citier_classes do |t|
-      end
-    end
-
-    class CitierClass < ActiveRecord::Base
-      acts_as_citier
-    end
-    
-    class NonCitierClass < ActiveRecord::Base
-      acts_as_citier
-    end
+ActiveRecord::Schema.define do
+  create_table :citier_classes do |t|
   end
-
-  it "sucessfully adds all citier methods to active record" do
-    citier_methods = Citier::Base::ClassMethods.instance_methods
-    active_record_methods = ActiveRecord::Base.methods
-
-    citier_methods.each do |m|
-      active_record_methods.include?(m).should eq(true)
-    end
+  create_table :non_citier_classes do |t|
   end
-  
-  it "sucessfully adds all citier instance methods to active record instances" do
-    citier_methods = Citier::Base::InstanceMethods.instance_methods
-    citierclass_methods = CitierClass.new.methods
+end
 
-    citier_methods.each do |m|
-      citierclass_methods.include?(m).should eq(true)
+class CitierClass < ActiveRecord::Base
+  acts_as_citier
+end
+
+class NonCitierClass < ActiveRecord::Base
+  acts_as_citier
+end
+
+describe "Implementing 'Acts as Citier'" do
+  describe "when adding methods to ActiveRecord::Base" do
+
+    it "sucessfully adds all citier methods to active record" do
+      citier_methods = Citier::Base::ClassMethods.instance_methods
+      active_record_methods = ActiveRecord::Base.methods
+
+      citier_methods.each do |m|
+        active_record_methods.include?(m).should eq(true)
+      end
+    end
+
+    it "sucessfully adds all citier instance methods to active record instances" do
+      citier_methods = Citier::Base::InstanceMethods.instance_methods
+      citierclass_methods = CitierClass.new.methods
+
+      citier_methods.each do |m|
+        citierclass_methods.include?(m).should eq(true)
+      end
     end
   end
 
