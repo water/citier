e48dc7adcea16cf7b896f5ddcf429bbd7d0359a7
Peter Hamilton <peter@inspiredpixel.net>
2012-01-26 02:13:04 +0000

    Added rails as a dependency and included it in the test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
+require 'rails'
 require 'rubygems'
+
 require 'bundler'
+
 begin
   Bundler.setup(:default, :development)
 rescue Bundler::BundlerError => e
@@ -7,14 +10,16 @@ rescue Bundler::BundlerError => e
   $stderr.puts "Run `bundle install` to install missing gems"
   exit e.status_code
 end
+
+
 require 'test/unit'
 require 'shoulda'
-
 require 'simplecov'
 SimpleCov.start
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'citier'
 
 class Test::Unit::TestCase
