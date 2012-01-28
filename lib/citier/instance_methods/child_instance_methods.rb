a7c5817e486144de7ef4daf0a9e5fc534c0e0db0
Peter Hamilton <peter@inspiredpixel.net>
2012-01-28 04:15:05 +0000

    Refactored the child instance methods and added a lot of comments to the save function

--- lib/citier/instance_methods/child_instance_methods.rb
+++ lib/citier/instance_methods/child_instance_methods.rb
@@ -1,21 +1,40 @@
 module Citier
   module ChildInstanceMethods
-
+    
+    # Handles the saving of a model.
+    # Seperates out the writable fields for the current class and those which
+    # belong 'to its parent classes. Then retrieves the writable table for each
+    # and saves the attributes.
+    #
+    # Returns false if the :validate option is set to false or the current class
+    # is not valid.
+    # * Runs callbacks for save/create/update just like ActiveRecord
+    # * Gather class attributes which belong to the parent model, isolating 
+    #   those which have changed so we don't make unecessary DB calls
+    # * Gather class attributes which belong to this class, not any parents and 
+    #   isolate those which have changed so we don't make unecessary DB calls
+    # * Create a new instance of the parent class, forcing it to take on the 
+    #   attributes we just extracted
+    # * The parent is set to be the same new_record setting as the current class
+    #   so if this is a new object, the parent knows it needs to be created as a 
+    #   new object too
+    # * Save the parent model. If the parent is also subclasses that's fine, 
+    #   it will also call this method.
+    # * Check to see if there are any attributes for the current class. If so, 
+    #   same drill as with the parent class
+    # * All parent and current classes have saved successfully. Set new_record 
+    #   to false and force the changed attributes to be none/empty so future 
+    #   changes will be recognised
     def save(options={})
       return false if (options[:validate] != false && !self.valid?)
-    
-      #citier_debug("Callback (#{self.inspect})")
+      
       citier_debug("SAVING #{self.class.to_s}")
-    
-      #AIT NOTE: Will change any protected values back to original values so any models onwards won't see changes.
-      # Run save and create/update callbacks, just like ActiveRecord does
+      
       self.run_callbacks(:save) do
         self.run_callbacks(self.new_record? ? :create : :update) do
-          #get the attributes of the class which are inherited from it's parent.
           attributes_for_parent = self.attributes.reject { |key,value| !self.class.superclass.column_names.include?(key) }
           changed_attributes_for_parent = self.changed_attributes.reject { |key,value| !self.class.superclass.column_names.include?(key) }
 
-          # Get the attributes of the class which are unique to this class and not inherited.
           attributes_for_current = self.attributes.reject { |key,value| self.class.superclass.column_names.include?(key) }
           changed_attributes_for_current = self.changed_attributes.reject { |key,value| self.class.superclass.column_names.include?(key) }
 
@@ -24,69 +43,35 @@ module Citier
           citier_debug("Attributes for #{self.class.to_s}: #{attributes_for_current.inspect}")
           citier_debug("Changed attributes for #{self.class.to_s}: #{changed_attributes_for_current.keys.inspect}")
 
-          ########
-          #
-          # Parent saving
-    
-          #create a new instance of the superclass, passing the inherited attributes.
           parent = self.class.superclass.new
-      
           parent.force_attributes(attributes_for_parent, :merge => true)
-          changed_attributes_for_parent["id"] = 0 # We need to change at least something to force a timestamps update.
           parent.force_changed_attributes(changed_attributes_for_parent)
-      
-          parent.id = self.id if id
-          parent.type = self.type
-    
+          
           parent.is_new_record(new_record?)
-      
-          # If we're root (AR subclass) this will just be saved as normal through AR. If we're a child it will call this method again. 
-          # It will try and save it's parent and then save itself through the Writable constant.
-          parent_saved = parent.save
-          self.id = parent.id
-
-          if !parent_saved
-            # Couldn't save parent class
-            citier_debug("Class (#{self.class.superclass.to_s}) could not be saved")
+          if !parent.save
+            citier_debug("Parent Class (#{self.class.superclass.to_s}) could not be saved")
             citier_debug("Errors = #{parent.errors.to_s}")
-            return false # Return false and exit run_callbacks :save and :create/:update, so the after_ callback won't run.
+            return false
           end
-    
-          #End of parent saving
-    
-          ######
-          ##
-          ## Self Saving
-          ##
 
-          # If there are attributes for the current class (unique & not inherited), save current model
-          if !attributes_for_current.empty?
+          if attributes_for_current.any?
             current = self.class::Writable.new
-        
+            
             current.force_attributes(attributes_for_current, :merge => true)
             current.force_changed_attributes(changed_attributes_for_current)
-        
+            
             current.id = self.id
             current.is_new_record(new_record?)
-      
-            current_saved = current.save
             
-            current.after_save_change_request if current.respond_to?('after_save_change_request') #Specific to an app I'm building
-
-            if !current_saved
+            if !current.save
               citier_debug("Class (#{self.class.superclass.to_s}) could not be saved")
               citier_debug("Errors = #{current.errors.to_s}")
-              return false # Return false and exit run_callbacks :save and :create/:update, so the after callback won't run.
+              return false
             end
-          end  
-
-          # at this point, parent_saved && current_saved
+          end
           
-          is_new_record(false) # This is no longer a new record
-
-          self.force_changed_attributes({}) # Reset changed_attributes so future changes will be tracked correctly
-          
-          # No return, because we want the after callback to run.
+          is_new_record(false)
+          self.force_changed_attributes({})
         end
       end
       return true
