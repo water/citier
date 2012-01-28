a7123831ce2f84b5141cf1f98554a439728c42b9
Peter Hamilton <peter@inspiredpixel.net>
2012-01-28 03:38:31 +0000

    Updated the create_citier_view function and added documentation

--- lib/citier/general_instance_methods.rb
+++ lib/citier/general_instance_methods.rb
@@ -1,14 +1,28 @@
 module Citier
   module InstanceMethods
-    def is_new_record(state)
-      @new_record = state
+    
+    # Sets if the current object is a new record or not
+    def is_new_record(b)
+      @new_record = b
     end
-
+    
+    # Creates a view by pulling in the relevant fields for the current class
+    # from all it's parent classes.
+    # Keeping a seperate view means we read all attributes from one table which
+    # is much faster than pulling them all in seperately using model-logic
+    # 
+    # The basic steps are as follows:
+    # * Reset all cached column information for this and all parent classes
+    # * Gather all non-id columns for the current class
+    # * Gather all non-id columns for the parents class - note: if the parent 
+    #   class is itself a subclass, it will retrieve these from it's view
+    # * Obtain the (current) readable and writable table for the current class
+    # * Obtain the table for the parent class
+    # * Generate a view named after the default table name for the class which 
+    #   pulls in columns from its own writable table and the readable table for
+    #   it's parent based on the columns retrieved earlier. 
+    #
     def create_citier_view(klass)
-      # function for creating views for migrations 
-      # flush any column info in memory
-      # Loops through and stops once we've cleaned up to our root class.
-      # We MUST user Writable as that is the place where changes might reside!
       reset_class = klass::Writable 
       until reset_class == ActiveRecord::Base
         citier_debug("Resetting column information on #{reset_class}")
@@ -18,13 +32,11 @@ module Citier
 
       self_columns = klass::Writable.column_names.select{ |c| c != "id" }
       parent_columns = klass.superclass.column_names.select{ |c| c != "id" }
-      columns = parent_columns+self_columns
+      columns = parent_columns + self_columns
       self_read_table = klass.table_name
       self_write_table = klass::Writable.table_name
       parent_read_table = klass.superclass.table_name
-      sql = "CREATE VIEW #{self_read_table} AS SELECT #{parent_read_table}.id, #{columns.join(',')} FROM #{parent_read_table}, #{self_write_table} WHERE #{parent_read_table}.id = #{self_write_table}.id" 
-
-      #Use our rails_sql_views gem to create the view so we get it outputted to schema
+      
       create_view "#{self_read_table}", "SELECT #{parent_read_table}.id, #{columns.join(',')} FROM #{parent_read_table}, #{self_write_table} WHERE #{parent_read_table}.id = #{self_write_table}.id" do |v|
         v.column :id
         columns.each do |c|
